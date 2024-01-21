9d4e981f4165902adec2d0be25225de00bb293a7
MAhsanOfficial <155831296+MAhsanOfficial@users.noreply.github.com>
2024-01-21 01:14:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("Asalam Walikum")
+
+let userName = prompt('Whats Your Name?')
+alert(  "" +  userName +"..." + 'Welcome to This My WebPage' )
+
+
+let age = prompt('Whats Your Age ?')
+
+console.log('named')
+console.log('age')
+
+
+if (age <= 25){
+    console.log('Your Are Student');
+}
+
+else{
+    console.log('You Are Not Student');
+}
